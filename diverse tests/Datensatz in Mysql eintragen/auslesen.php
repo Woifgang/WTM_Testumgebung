1ab07739f1f5f928d339ef8744a50cc47ec854ae
Woifgang <wolfgang.aschenbrenner@mail.de>
2017-11-16 21:07:51 +0100

    Daten werden aus Datenbank empfangen

---
+++ diverse tests/Datensatz in Mysql eintragen/auslesen.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Daten auslesen</title>
+        
+    </head>
+    <body>
+        <?php
+            $server = "127.0.0.3";
+            $benutzer = "db289129_217";
+            $kennwort = "ps:s5meFg2mV";
+            $datenbank = "db289129_217";
+        
+            $verbindung = mysqli_connect ($server, $benutzer, $kennwort, $datenbank);
+        
+            $databaseAbfrage = "SELECT * FROM woidtrailmap";
+        
+            $databaseErgebnis = mysqli_query($verbindung, $databaseAbfrage);
+            
+            if(!$databaseErgebnis){
+                die('Ungültige Abfrage: ' . mysqli_error());
+            }
+        
+            echo '<table border ="1">';
+                while ($zeile = mysqli_fetch_array($databaseErgebnis, MYSQLI_ASSOC)){
+                    echo "<tr>";
+                    echo "<td>". $zeile['laengengrad'] . "</td>";
+                    echo "<td>". $zeile['breitengrad'] . "</td>";
+                    echo "<td>". $zeile['ueberschrift'] . "</td>";
+                    echo "<td>". $zeile['beschreibung'] . "</td>";
+                    echo "<td>". $zeile['pfadGPX'] . "</td>";
+                    echo "</tr>";
+                }
+            echo "</table>";
+            
+            mysqli_free_result($databaseErgebnis);
+        ?>
+    
+    </body>
+
+</html>
--- diverse tests/Datensatz in Mysql eintragen/datenbank.php
+++ diverse tests/Datensatz in Mysql eintragen/datenbank.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <?php 
-            $server = "127.0.0.3";
+             $server = "127.0.0.3";
              $Benutzer = "db289129_217";
              $Kennwort = "ps:s5meFg2mV";
              $Datenbank = "db289129_217";
